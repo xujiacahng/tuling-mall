4c8d8b339df4347401c1b827eb90376d28cdc12c
xujc <1178494423@qq.com>
2022-01-07 15:17:34 +0800

    修改订单监控

--- tulingmall-gateway/src/main/java/com/tuling/tulingmall/MallGatewayApplication.java
+++ tulingmall-gateway/src/main/java/com/tuling/tulingmall/MallGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.tuling.tulingmall;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
